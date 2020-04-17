37244e71e9b704ef5446870dbe1518d4e223bc31
Ashan <ashanjiang@foxmail.com>
2020-04-17 15:55:16 +0800

    P1802 复习01背包

---
+++ P1802/P1802.cpp
@@ -0,0 +1,40 @@
+﻿//P1802 五倍经验日（还是01背包）
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int win[1005], lose[1005], use[1005];
+int dp[1005][1005];
+
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	int a, b, c;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a >> b >> c;
+		lose[i] = a;
+		win[i] = b;
+		use[i] = c;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j <= x; j++)
+		{
+			if (j < use[i]) //药不够，直接落败
+			{
+				dp[i][j] = dp[i - 1][j] + lose[i];
+			}
+			else
+			{
+				dp[i][j] = max(dp[i - 1][j] + lose[i], dp[i - 1][j - use[i]] + win[i]);
+			}
+		}
+	}
+
+	cout << 5ll * dp[n][x] << endl;
+
+	return 0;
+}
