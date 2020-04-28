2362652ad57ee70209683b17574c776cfff59132
Ashan <ashanjiang@foxmail.com>
2020-04-28 11:31:26 +0800

    P3811 乘法逆元模板 线性算法

--- P3811/P3811.cpp
+++ P3811/P3811.cpp
@@ -1,6 +1,5 @@
 ﻿//P3811 乘法逆元模板（常用于模数除法）
 #include <cstdio>
-#include <iostream>
 typedef long long ll;
 
 using namespace std;
@@ -18,7 +17,7 @@ ll binPow(ll a, ll b)
 	return res;
 }
 
-//乘法逆元
+//乘法逆元，费马小定理
 //不能直接先算幂再取模，否则会溢出，需要每一步都取模
 ll inverseElement(ll a, ll p, ll mod)
 {
@@ -32,14 +31,26 @@ ll inverseElement(ll a, ll p, ll mod)
 	return res % mod;
 }
 
+ll inv[20000528 + 5];
+
 int main()
 {
 	//给定n,p求1~n中所有整数在模p意义下的乘法逆元。
-	ll n, p;
+	/*ll n, p;
 	cin >> n >> p;
 	for (int i = 1; i <= n; i++)
 	{
 		printf("%lld\n", inverseElement(i, p - 2, p));
+	}*/
+	int n, p;
+	scanf("%d%d", &n, &p);
+	inv[1] = 1;//1的逆元恒为1
+	printf("%d\n", inv[1]);
+	for (int i = 2; i <= n; i++)
+	{
+		// 线性推逆元，O(n)
+		inv[i] = (ll)p - (p / i) * inv[p % i] % p;
+		printf("%d\n", inv[i]);
 	}
 	return 0;
 }
