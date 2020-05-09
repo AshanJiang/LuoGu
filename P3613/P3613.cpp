4ff9e678e335610302a20cb16a57a0bb24efcabc
AshanJiang <ashanjiang@foxmail.com>
2020-05-09 15:03:20 +0800

    P3613 map的使用

---
+++ P3613/P3613.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+map <int, map<int, int> > locker;
+
+int main()
+{
+	int n, q, a, b, c, d;
+	//第一行2个整数n和q，寄包柜个数和询问次数。
+	cin >> n >> q;
+	for (int i = 0; i < q; i++)
+	{
+		cin >> a;
+		if(a==1)
+		{
+			cin >> b >> c >> d;
+			locker[b][c] = d;
+		}
+		else
+		{
+			cin >> b >> c;
+			cout << locker[b][c] << '\n';
+		}
+	}
+	return 0;
+}
