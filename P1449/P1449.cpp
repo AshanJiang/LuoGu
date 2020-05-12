f155815411a891f0e9f37b076783da7649b0deae
AshanJiang <ashanjiang@foxmail.com>
2020-05-12 09:46:03 +0800

    P1449 后缀表达式

---
+++ P1449/P1449.cpp
@@ -0,0 +1,63 @@
+﻿//P1449 后缀表达式
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	string expr, token = "";
+	cin >> expr;
+	stack <int> s;
+	int op1, op2;
+	for (char& c : expr)
+	{
+		if (c == '+')
+		{
+			op1 = s.top();
+			s.pop();
+			op2 = s.top();
+			s.pop();
+			s.push(op2 + op1);
+		}
+		else if (c == '-')
+		{
+			op1 = s.top();
+			s.pop();
+			op2 = s.top();
+			s.pop();
+			s.push(op2 - op1);
+		}
+		else if (c == '*')
+		{
+			op1 = s.top();
+			s.pop();
+			op2 = s.top();
+			s.pop();
+			s.push(op2 * op1);
+		}
+		else if (c == '/')
+		{
+			op1 = s.top();
+			s.pop();
+			op2 = s.top();
+			s.pop();
+			s.push(op2 / op1);
+		}
+		else
+		{
+			if (c != '.')
+			{
+				token += c;
+			}
+			else
+			{
+				s.push(stoi(token));
+				token = "";
+			}
+		}
+	}
+	cout << s.top() << '\n';
+	return 0;
+}
